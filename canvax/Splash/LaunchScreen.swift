00687f2d4146a762904277a52783b5222e152546
valome <180098651+is-valome@users.noreply.github.com>
2025-05-26 16:58:10 +0000

    App Icon & Logo

---
+++ canvax/Splash/LaunchScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreen.swift
+//  canvax
+//
+//  Created by valome on 5/26/25.
+// Initial start
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LaunchScreen()
+}
