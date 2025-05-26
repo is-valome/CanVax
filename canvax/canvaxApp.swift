c0f0185a095aa9791fe3c0a613e3483ed049682f
valome <180098651+is-valome@users.noreply.github.com>
2025-05-26 11:52:05 +0000

    Initial commit

---
+++ canvax/canvaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  canvaxApp.swift
+//  canvax
+//
+//  Created by valome on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct canvaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
